2bd8c9024930ccf177a56da23ea44b99d5bcc0cd
unknown <fregenal.maurojoaquin@iesconcepcion.edu.ar>
2025-02-14 12:28:18 -0300

    Tercer commit agregacion de botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 import Contador from "./components/Contador";
+import Boton from "./components/Boton";
 
 function App() {
   const [contador, setContador] = useState(0);
 
-  const contadordinamico = () => {
-    setContador (contador +1);
-  }
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
 
+  const decrementar = () => {
+    if (contador > 0) {
+      setContador(contador - 1);
+    }
+  };
+  const reiniciar = () => {
+    setContador(0);
+  };
   return (
     <>
       <h1>Contador con Props</h1>
-      <Contador contInicial = {contador}/>
+      <Contador contInicial={contador} />
+      <Boton btnIncrementar={incrementar} nombre={"Incrementar"} />
+      <Boton btnIncrementar={decrementar} nombre={"Decrementar"} />
+      <Boton btnIncrementar={reiniciar} nombre={"Reiniciar"} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Boton.jsx
@@ -0,0 +1,12 @@
+function Boton({btnIncrementar, nombre}) {
+
+    return (
+        <div>
+            <button onClick={btnIncrementar}>{nombre}</button>
+        </div>
+    )
+}
+export default Boton
+
+
+
